bd8c1f5cf57111b947ae5a3a05c2a69df22c4288
adarista012 <ad.arista012@gmail.com>
2023-11-05 19:51:01 -0400

    add/firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:just_play/app/app.dart';
+import 'package:just_play/firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
